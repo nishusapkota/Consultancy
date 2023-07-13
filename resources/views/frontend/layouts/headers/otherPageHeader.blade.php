c6c09257363c67601a5c9d14bb1198e1dabbe2b3
saroj bhujel <saroj.spell@gmail.com>
2023-07-13 13:10:46 +0545

    added upper header part

--- resources/views/frontend/layouts/headers/otherPageHeader.blade.php
+++ resources/views/frontend/layouts/headers/otherPageHeader.blade.php
@@ -1,4 +1,23 @@
 <header class="main-header style-one style-six">
+    <div class="header-top">
+            <div class="auto-container">
+                <div class="top-inner clearfix">
+                    <ul class="info top-left pull-left">
+                        <li><i class="fas fa-map-marker-alt"></i>838 Andy Street, Madison, NJ 08003</li>
+                        <li><i class="fas fa-headphones"></i>Support <a href="tel:01005200369">0100 5200 369</a></li>
+                    </ul>
+                    <div class="top-right pull-right">
+                        <ul class="social-links clearfix">
+                            <li><a href="index.html"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="index.html"><i class="fab fa-google-plus-g"></i></a></li>
+                            <li><a href="index.html"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="index.html"><i class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href="index.html"><i class="fab fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     <div class="header-lower">
         <div class="auto-container">
             <div class="outer-box clearfix">
